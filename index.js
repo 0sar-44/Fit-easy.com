a1d15955a0863f292e800083bf2c823f27850852
sanket raj <139071991+0sar-44@users.noreply.github.com>
2023-09-06 03:05:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector(".mobile-btn").addEventListener("click", function () {
+    document.querySelector(".menu").classList.toggle("active");
+    <button onclick="window.open('https://www.3schools.in','_self');">
+    Open tab
+  </button>
+  });
